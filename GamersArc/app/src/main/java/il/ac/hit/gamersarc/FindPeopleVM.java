d3f01846b0ff4fa91306f9e2decb4a649b4960ab
nati888 <nati1143@gmail.com>
2021-06-30 00:11:16 +0300

    cant find players

--- GamersArc/app/src/main/java/il/ac/hit/gamersarc/FindPeopleVM.java
+++ GamersArc/app/src/main/java/il/ac/hit/gamersarc/FindPeopleVM.java
@@ -43,7 +43,7 @@ public class FindPeopleVM extends AndroidViewModel {
     private DataBaseClass dataBaseClass = DataBaseClass.getInstance();
     private ArrayList<User> usersFromDatabase = new ArrayList<>();
     private UserPreferences userPreferences;
-    private final String API_TOKEN_KEY = "AAAA1Zp43-k:APA91bG2LGRbDitz5cKuyq8NhaWiCa_SCMx_CdVm2MeAwFTxCJpEpf1QMZt2hecz0etzk2ggiTp9np0EwCEfeuO-JQpknA5Wc7MbnUwj80rq9gtcvzR_E1b0s6_MnBL7D9JOrs0YjPh5";
+    private final String API_TOKEN_KEY = "AAAA1Zp43-k:APA91bFfzWrTpaf6Sy3xtAbXz715LWVRxguG9NsdxLuAmwebw2sOiwA8uwCd-mfTlmr7kiyWwe6U3k0hnA7Gk_HL3RAa8oFaoStYsTy3sp6DVymQROiQiY-abDniiQNM1z6UgY-TAu_b";
 
     private MutableLiveData<Boolean> swipeLayoutBool = new MutableLiveData<>();
 
